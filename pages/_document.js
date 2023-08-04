a4ab91084108b18fdcc68245c653a68939f09def
Elvin Ebby <elvin.ebby@gmail.com>
2023-08-04 18:10:46 -0400

    Title Page
     Added Background and Header

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{padding: 0,margin:0}}>
       <Head />
-      <body>
+      <body style={{padding: 0,margin:0}}>
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import React from "react";
+import styles from "@/styles/Page.module.css";
+
 export default function Page() {
-    return <h1> hello world </h1>
+  return (
+    
+    <div className={styles.container}>
+      <h1 className={styles.titly}style={{padding:0, margin:0}}>EcoPrint</h1>
+    </div>
+   
+  );
 }
+
