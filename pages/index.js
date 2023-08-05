0b6b24cdac58fd262e6d79dbc0a659b894325748
Elvin Ebby <elvin.ebby@gmail.com>
2023-08-05 16:28:17 -0400

    Added Logo and Login Btn

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import styles from "@/styles/Page.module.css";
+import { Button } from "@mui/material";
+import { styled } from "@mui/system";
+
+const GradientButton = styled(Button)`
+  background: linear-gradient(45deg, #1b4e50 30%, #1b4e50 90%);
+  border-radius: 3px;
+  border: 0;
+  color: white;
+  height: 48px;
+  padding: 0 30px;
+
+`;
 
 export default function Page() {
   return (
     
     <div className={styles.container}>
-      <h1 className={styles.titly}style={{padding:0, margin:0}}>EcoPrint</h1>
+
+        <GradientButton className={styles.btn} variant="contained">Login/Register</GradientButton>
+
+        <h1 className={styles.titly}style={{padding:0, margin:0}}>EcoPrint</h1>
     </div>
+
+
    
   );
 }
