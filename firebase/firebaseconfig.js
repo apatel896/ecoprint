aca8184fd729e4fd1fc13b8bfa11797445813a9b
Aryan Patel <patelaryan1013@gmail.com>
2023-08-05 17:13:01 -0400

    Added signup page
    Signup still not fully functional, just testing out google firebase api

---
+++ firebase/firebaseconfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwSRgAOR_0gJShxKEx8Cc6UFCidMEfAx0",
+  authDomain: "ecoprint-658af.firebaseapp.com",
+  projectId: "ecoprint-658af",
+  storageBucket: "ecoprint-658af.appspot.com",
+  messagingSenderId: "914139964665",
+  appId: "1:914139964665:web:3a188ff3469f849aa9ee28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ pages/signup.js
@@ -0,0 +1,57 @@
+'use client'
+import app from "../firebase/firebaseconfig.js"
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React from "react";
+import { useRouter } from 'next/navigation'
+
+const auth = getAuth(app);
+async function signUp(email, password) {
+    let result = null,
+        error = null;
+    try {
+        result = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
+
+
+function Page() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const router = useRouter()
+
+    const handleForm = async (event) => {
+        event.preventDefault()
+
+        const { result, error } = await signUp(email, password);
+
+        if (error) {
+            return console.log(error)
+        }
+
+        // else successful
+        console.log(result)
+        return;
+    }
+    return (<div className="wrapper">
+        <div className="form-wrapper">
+            <h1 className="mt-60 mb-30">Sign up</h1>
+            <form onSubmit={handleForm} className="form">
+                <label htmlFor="email">
+                    <p>Email</p>
+                    <input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" />
+                </label>
+                <label htmlFor="password">
+                    <p>Password</p>
+                    <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" />
+                </label>
+                <button type="submit">Sign up</button>
+            </form>
+        </div>
+    </div>);
+}
+
+export default Page;
